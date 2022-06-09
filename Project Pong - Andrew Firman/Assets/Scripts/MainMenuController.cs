23ceda0fa39c7a39db305e935ca827bd0fe60d2e
andrewfirmansap <34363819+andrewfirmansap@users.noreply.github.com>
2022-06-09 20:02:13 +0700

    add DebugLog when PlayButton pressed

--- Project Pong - Andrew Firman/Assets/Scripts/MainMenuController.cs
+++ Project Pong - Andrew Firman/Assets/Scripts/MainMenuController.cs
@@ -8,6 +8,8 @@ public class MainMenuController : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("Game");
+        Debug.Log("Created by andrew - firman - saputra - 149251970101 - 61");
+        
     }
     public void OpenCredit()
     {
